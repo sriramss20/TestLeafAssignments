2495ac6bd9b86b6d9f68f5efe4360c0da9bd10bd
sriramss20 <62903561+sriramss20@users.noreply.github.com>
2020-03-31 00:14:08 +0530

    Add files via upload

---
+++ Android.java
@@ -0,0 +1,27 @@
+package week1assignment;
+
+public class Android {
+
+	
+	long a=400;
+	boolean answer=true;
+	String name="sriram";
+	int b=20;
+	float d=20.02f;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Android obj=new Android();
+		
+		System.out.println(obj.a);
+		
+		System.out.println(obj.answer);
+		System.out.println(obj.name);
+		System.out.println(obj.b);
+		
+		float d2 = obj.d;
+System.out.println(d2);
+	}
+
+}

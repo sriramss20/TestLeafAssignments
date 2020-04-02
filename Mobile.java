cd50b16d933c40942da4bd2b79f7a1421dc22907
sriramss20 <62903561+sriramss20@users.noreply.github.com>
2020-04-02 15:21:32 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class Mobile {
+
+	boolean gearless=true;
+	
+	
+	String name="bmw";
+	
+	
+	
+	int age=10;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("am ready");
+		
+		
+		Mobile mob=new Mobile();
+//		String name2 = mob.name;
+		String name2 = mob.name;
+		
+		int age2 = mob.age;
+//		String name1=mob.name;
+//		System.out.println(name1);
+//		
+		boolean gearless2 = mob.gearless;
+//		boolean gearless1=mob.gearless;
+//		System.out.println(gearless1);
+//		
+//		int age=mob.age;
+//		System.out.println(age);
+	}
+
+}
